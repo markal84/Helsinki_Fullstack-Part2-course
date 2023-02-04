71473581a5015dcd1c7853a7f6f4ab5f07772cbb
Mar Kal <marc@example.com>
2023-02-04 17:36:15 +0100

    feat: finish 2.9 task

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -2,12 +2,19 @@ import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 const App = () => {
-  const phonebook = [{ id: 1, name: "Arto Hellas" }];
+  const phonebook = [
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ];
+
   const id = uuidv4();
 
   const [persons, setPersons] = useState(phonebook);
   const [newName, setNewName] = useState("please enter name...");
   const [newNumber, setNewNumber] = useState("phone number...");
+  const [filter, setFilter] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -27,12 +34,37 @@ const App = () => {
     }
   };
 
+  // show filtered results if there is value in filter input
+  const isPersonsFiltered =
+    filter === ""
+      ? persons
+      : persons.filter((person) =>
+          person.name.toLowerCase().includes(filter.toLowerCase())
+        );
+  /*
+      const isPersonsFiltered = () => {
+    if (showAll) {
+      console.log("returning persons list");
+      return persons;
+    } else {
+      console.log("returning filtered persons list", filter);
+      return persons.filter((person) =>
+        person.name.toLowerCase().includes(filter.toLowerCase())
+      );
+    }
+  };
+  */
+
   const handleAddName = (e) => {
     setNewName(e.target.value);
   };
 
   const handleAddNumber = (e) => {
     setNewNumber(e.target.value);
+  };
+
+  const handleFilter = (e) => {
+    setFilter(e.target.value);
   };
 
   const clearName = () => {
@@ -44,11 +76,10 @@ const App = () => {
   };
 
   return (
-    <div>
-      <div>
-        debug: {newName} {newNumber}
-      </div>
+    <>
       <h2>Phonebook</h2>
+      <div>debug: filter - {filter}</div>
+      <input type="text" value={filter || ""} onChange={handleFilter} />
       <form onSubmit={handleSubmit}>
         <div>
           name:{" "}
@@ -74,13 +105,13 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map((person) => (
+        {isPersonsFiltered.map((person) => (
           <li key={person.id}>
             Name: {person.name} : {person.number}
           </li>
         ))}
       </ul>
-    </div>
+    </>
   );
 };
 
