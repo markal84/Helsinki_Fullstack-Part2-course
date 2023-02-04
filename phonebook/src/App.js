c82d370734bcd69d503628f376ea8a4462180e24
Mar Kal <marc@example.com>
2023-02-04 11:19:19 +0100

    feat: finish 2.6 task

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,22 +1,47 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
-  const [newName, setNewName] = useState("");
+  const phonebook = [{ id: 1, name: "Arto Hellas" }];
+  const id = Math.floor(Math.random() * 10); // not great but for that small app is ok
+
+  const [persons, setPersons] = useState(phonebook);
+  const [newName, setNewName] = useState("please enter name...");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newPerson = {
+      id: id,
+      name: newName,
+    };
+
+    setPersons(persons.concat(newPerson));
+    setNewName("");
+    console.log("person added :", newName);
+  };
+
+  const handleAddName = (e) => {
+    setNewName(e.target.value);
+  };
 
   return (
     <div>
+      <div>debug: {newName}</div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
-          name: <input />
+          name:{" "}
+          <input type="text" value={newName || ""} onChange={handleAddName} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <ul>
+        {persons.map((person) => (
+          <li key={person.id}>{person.name}</li>
+        ))}
+      </ul>
     </div>
   );
 };
